5d2604813da0aaaed6b60f7ccf6ac2d4d8297f36
hugoderiii <hugoderiii@gmail.com>
2018-03-19 23:44:27 +0100

    add buy on mobile

---
+++ buyOnMobile.js
@@ -0,0 +1,87 @@
+const puppeteer = require('puppeteer');
+
+class BuyOnMobile {
+
+    async buyProduct(product, sizeId, prefs, isTesting, finishCallback, retryCallback) {
+        var args = {args: ['--no-sandbox']};
+        isTesting ? args.headless = false : args.headless = true;
+        var browser = await puppeteer.launch(args);
+        try {
+
+            const page = await browser.newPage();
+            await page.goto(product.link);
+
+            const SIZE_SELECTOR = "#size-options";
+            await page.select(SIZE_SELECTOR, sizeId.toString());
+
+            const ADD_PRODUCT_SELECTOR = '#cart-update>cart-button';
+            const SOLD_OUT_SELECTOR_SELECTOR = '#cart-update>.cart-button.sold-out';
+            const soldOutButton = await page.$(SOLD_OUT_SELECTOR_SELECTOR);
+            if (soldOutButton) {
+                await browser.close();
+                return finishCallback();
+            }
+
+            await page.click(ADD_PRODUCT_SELECTOR);
+
+            await page.waitFor(1 * 1000);
+
+            const CHECKOUT_SELECTOR = '#checkout-now';
+
+            await page.click(CHECKOUT_SELECTOR);
+
+            await page.waitFor(1 * 1000);
+
+            const NAME_SELECTOR = '#order_billing_name';
+            const EMAIL_SELECTOR = '#order_email';
+            const TEL_SELECTOR = '#order_tel';
+            const ADRESS_SELECTOR = '#order_billing_adress';
+            const CITY_SELECTOR = '#order_billing_city';
+            const ZIP_SELECTOR = '#order_billing_zip';
+            const COUNTRY_SELECTOR = '#order_billing_country';
+            const CARD_TYPE_SELECTOR = '#credit_card_type';
+            const CARD_NUMBER_SELECTOR = '#credit_card_n';
+            const CARD_MONTH_SELECTOR = '#credit_card_month';
+            const CARD_YEAR_SELECTOR = '#credit_card_year';
+            const CARD_VVAL_SELECTOR = '#credit_card_cvv';
+            const TERMS_SELECTOR = '#order_terms'
+
+            await page.$eval(NAME_SELECTOR, (el, value) => el.value = value, prefs.name);
+            await page.$eval(EMAIL_SELECTOR, (el, value) => el.value = value, prefs.email);
+            await page.$eval(TEL_SELECTOR, (el, value) => el.value = value, prefs.tel);
+            await page.$eval(ADRESS_SELECTOR, (el, value) => el.value = value, prefs.adress);
+            await page.$eval(CITY_SELECTOR, (el, value) => el.value = value, prefs.city);
+            await page.$eval(ZIP_SELECTOR, (el, value) => el.value = value, prefs.zip);
+            await page.$eval(COUNTRY_SELECTOR, (el, value) => el.value = value, prefs.country);
+            await page.$eval(CARD_TYPE_SELECTOR, (el, value) => el.value = value, prefs.cardType);
+            await page.$eval(CARD_NUMBER_SELECTOR, (el, value) => el.value = value, prefs.cardNumber);
+            await page.$eval(CARD_MONTH_SELECTOR, (el, value) => el.value = value, prefs.cardMonth);
+            await page.$eval(CARD_YEAR_SELECTOR, (el, value) => el.value = value, prefs.cardYear);
+            await page.$eval(CARD_VVAL_SELECTOR, (el, value) => el.value = value, prefs.cardVval);
+
+            // await page.evaluate('checkoutAfterCaptcha();');
+
+            // await page.click(TERMS_SELECTOR);
+
+            await page.waitFor(3 * 1000);
+
+            await browser.close();
+
+            finishCallback();
+        } catch (e) {
+            console.log(e);
+            try {
+                browser.close();
+            } catch (expection) {}
+            retryCallback();
+        }
+    }
+
+    stop () {
+        try {
+            browser.close();
+        } catch (expection) {}
+    }
+}
+
+exports.BuyOnMobile = BuyOnMobile;
