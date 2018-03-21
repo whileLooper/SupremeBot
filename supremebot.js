e8efc72d6eb421f4c79e6c766b2c4bc5454d8f69
Paul Bühler <paul.buehler@it-designers.de>
2018-03-21 15:07:31 +0100

    implement captcha pool

--- supremebot.js
+++ supremebot.js
@@ -4,18 +4,18 @@ var jsonfile = require('jsonfile');
 var captchaSolver = require('./captchasolver');
 var buyOnMobile = require('./buyOnMobile');
 const WORKER_COUNT = 1;
-const WORKER_TIMEOUT_MS = 1000 * 60 * 12;
+const WORKER_TIMEOUT_MS = 1000 * 60 * 2;
 var workers = [];
 var isTesting = process.argv.length > 2 ? process.argv [2] === 'testing' : false;
 
-function intializeWorkers(prefs, solver) {
+function intializeWorkers(prefs, captchaPool) {
 	workers = [];
 	for (var i = 0; i < WORKER_COUNT; i++)
-		workers.push(new SupremeWorker(prefs, solver));
+		workers.push(new SupremeWorker(prefs, captchaPool));
 }
 
 function assignToWorker(productDefinition, callback) {
-	isAssigned = workers.some(worker => {
+	var isAssigned = workers.some(worker => {
 		if (!worker.hasWork()) {
 			worker.assignProduct(productDefinition, callback);
 			return true;
@@ -32,7 +32,8 @@ function startDrop(mainCallback) {
 	var prefs = jsonfile.readFileSync("./prefs.json");
 	var droplist = jsonfile.readFileSync("./droplist.json");
 	var solver = new captchaSolver.CaptchaSolver(prefs.antiCaptchaKey);
-	intializeWorkers(prefs, solver);
+	var captchaPool = new CaptchaPool(solver);
+	intializeWorkers(prefs, captchaPool);
 
 	async.each(droplist, (itemDefinition, callback) => {
 		assignToWorker(itemDefinition, callback);
@@ -43,14 +44,51 @@ function startDrop(mainCallback) {
 	});
 }
 
+class CaptchaPool {
+
+	constructor(solver) {
+		this.inProgress = 0;
+		this.solver = solver;
+		this.captchaList = [];
+		this.updateCaptchaPool();
+	}
+
+	updateCaptchaPool() {
+		if (this.stopped)
+			return;
+		this.captchaList = this.captchaList.filter(captcha => !isTimestampOlderThan(captcha.timestamp, 1000 * 60 * 2));
+		const onlyNewCaptchas = this.captchaList.filter(captcha => !isTimestampOlderThan(captcha.timestamp, 1000 * 60));
+		if (onlyNewCaptchas.length + this.inProgress < WORKER_COUNT * 2) {
+			this.requestNewCaptcha();
+		}
+		setTimeout(() => this.updateCaptchaPool(), 1000 * 5);
+	}
+
+	requestNewCaptcha() {
+		this.inProgress++;
+		this.solver.solveCaptcha('https://www.supremenewyork.com/checkout', '6LeWwRkUAAAAAOBsau7KpuC9AV-6J8mhw4AjC3Xz', captchaToken => {
+			console.log("New captcha token: " + captchaToken);
+			this.captchaList.push({token:captchaToken, timestamp:Date.now()});
+			this.inProgress--;
+		}, true);
+	}
+
+	getCaptcha() {
+		return this.captchaList.length > 0 ? this.captchaList.shift() : null;
+	}
+
+	stop () {
+		this.stopped = true;
+	}
+}
+
 class SupremeWorker {
 
-	constructor(prefs, solver) {
+	constructor(prefs, captchaPool) {
 		this.prefs = prefs;
-		this.solver = solver;
+		this.captchaPool = captchaPool;
 		this.buyApi = new buyOnMobile.BuyOnMobile();
-		this.startTimestampMS = Date().now;
-		this.getCaptchaToken();
+		this.startTimestampMS = Date.now();
 	}
 
 	hasWork() {
@@ -64,34 +102,18 @@ class SupremeWorker {
 	}
 
 	checkForTimeout() {
-		const differenceMS = Date.now() - this.startTimestampMS
+		const differenceMS = Date.now() - this.startTimestampMS;
 		if (differenceMS > WORKER_TIMEOUT_MS)
 			this.stop();
 	}
 
-	getCaptchaToken() {
+	checkForProduct() {
 		this.checkForTimeout();
 		if (this.stopped === true)
 			return;
 
-		const startTimestampMs = Date.now();
-		this.solver.solveCaptcha('https://www.supremenewyork.com/checkout', '6LeWwRkUAAAAAOBsau7KpuC9AV-6J8mhw4AjC3Xz', captchaToken => {
-			console.log("New captcha token: " + captchaToken);
-			this.captchaToken = captchaToken;
-			const endTimestampMS = Date.now();
-			const difference = endTimestampMS - startTimestampMs;
-			var nextTokenInMS = 100 * 1000 - difference;
-			nextTokenInMS = nextTokenInMS > 0 ? nextTokenInMS : 0;
-			setTimeout(() => this.getCaptchaToken(), nextTokenInMS);
-		}, true);
-	}
-
-	checkForProduct() {
-		if (this.stopped === true)
-			return;
-
 		supremeMobile.findItem(this.productDefinition.category, this.productDefinition.keywords, product => {
-			if (product && this.captchaToken) {
+			if (product && this.getCaptcha ()) {
 				const availableStyles = this.getAvailableStyles(product);
 				if (availableStyles.length > 0)
 					this.buyProduct(product, availableStyles);
@@ -103,10 +125,21 @@ class SupremeWorker {
 		});
 	}
 
+	getCaptcha () {
+		if (this.captcha && !isTimestampOlderThan(this.captcha.timestamp, 1000* 100)) {
+			return this.captcha;
+		} else {
+			this.captcha = this.captchaPool.getCaptcha();
+			return this.captcha;
+		}
+	}
+
 	buyProduct(product, availableStyles) {
 		this.buyApi.buyProduct(product,
 			availableStyles,
-			this.prefs, isTesting,
+			this.prefs,
+			this.captcha.token,
+			isTesting,
 			() => this.finishWork(),
 			() => this.checkForProduct());
 	}
@@ -121,7 +154,8 @@ class SupremeWorker {
 		this.productDefinition.sizes = this.productDefinition.sizes.map(style => style.toLowerCase());
 		return product.styles.map(style => {
 			const sizes = style.sizes.filter(size => {
-				return this.productDefinition.sizes.includes(this.formatStyle(size.name)) && size.stock_level == 1;
+				return this.productDefinition.sizes.includes(this.formatStyle(size.name))
+					&& size.stock_level === 1;
 			}).map(size => size.id + "");
 			return {
 				name: this.formatStyle(style.name),
@@ -132,7 +166,7 @@ class SupremeWorker {
 			.filter(style => this.productDefinition.styles.includes(style.name));
 	}
 
-	formatStyle(string) {
+	static formatStyle(string) {
 		string = encodeURI(string);
 		string = string.replace(/%EF%BB%BF/g, "");
 		return decodeURI(string).toLowerCase();
@@ -154,6 +188,10 @@ function waitForDrop() {
 	} else {
 		setTimeout(() => waitForDrop(), 10000);
 	}
+}
+
+function isTimestampOlderThan(timestamp, treshold) {
+	return Date.now() - timestamp > treshold;
 }
 
 if (isTesting)
