cc3cc39155be73df84ec9fd547d960d629442c65
hugoderiii <hugoderiii@gmail.com>
2018-04-09 21:11:08 +0200

    improve start script

--- startscript.bash
+++ startscript.bash
@@ -1,1 +1,7 @@
-node supremebot.js | tee logs.txt
+NOW=$(date +"%Y-%m-%d")
+
+trap 'pkill -f droplistFillerServer.js' SIGINT SIGTERM EXIT
+
+node droplistFillerServer.js &> "./frontend-$NOW.txt" &
+
+node supremebot.js | tee supreme-$NOW.txt
