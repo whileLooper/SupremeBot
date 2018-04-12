0c796d55e65c466da6342ae63529959fbdda4fbd
hugoderiii <hugoderiii@gmail.com>
2018-04-12 04:31:16 +0200

    fixed supreme desktp buy api

--- apis/buyProductPuppeteer.js
+++ apis/buyProductPuppeteer.js
@@ -7,7 +7,7 @@ class BuyProductPuppeteer {
         isTesting ? args.headless = false : args.headless = true;
         var browser = await puppeteer.launch(args);
         try {
-            const style = styles [0];
+            const style = styles[0];
             const sizeId = style.sizes[0];
 
             const page = await browser.newPage();
@@ -32,37 +32,39 @@ class BuyProductPuppeteer {
 
             await page.waitFor(1000);
 
-            const NAME_SELECTOR = '#order_billing_name';
-            const EMAIL_SELECTOR = '#order_email';
-            const TEL_SELECTOR = '#order_tel';
-            const ADRESS_SELECTOR = '#bo';
-            const CITY_SELECTOR = '#order_billing_city';
-            const ZIP_SELECTOR = '#order_billing_zip';
-            const COUNTRY_SELECTOR = '#order_billing_country';
-            const CARD_TYPE_SELECTOR = '#credit_card_type';
-            const CARD_NUMBER_SELECTOR = '#cnb';
-            const CARD_MONTH_SELECTOR = '#credit_card_month';
-            const CARD_YEAR_SELECTOR = '#credit_card_year';
-            const CARD_VVAL_SELECTOR = '#vval';
+            const NAME_SELECTOR = 'order_billing_name';
+            const EMAIL_SELECTOR = 'order_email';
+            const TEL_SELECTOR = 'order_tel';
+            const ADRESS_SELECTOR = 'bo';
+            const CITY_SELECTOR = 'order_billing_city';
+            const ZIP_SELECTOR = 'order_billing_zip';
+            const COUNTRY_SELECTOR = 'order_billing_country';
+            const CARD_TYPE_SELECTOR = 'credit_card_type';
+            const CARD_NUMBER_SELECTOR = 'cnb';
+            const CARD_MONTH_SELECTOR = 'credit_card_month';
+            const CARD_YEAR_SELECTOR = 'credit_card_year';
+            const CARD_VVAL_SELECTOR = 'vval';
+            const RECAPTCHA_RESPONSE_SELECTOR = 'g-recaptcha-response';
+            
             const TERMS_SELECTOR = '#order_terms'
-            const RECAPTCHA_RESPONSE_SELECTOR = '#g-recaptcha-response';
 
-            await page.$eval(NAME_SELECTOR, (el, value) => el.value = value, prefs.name);
-            await page.$eval(EMAIL_SELECTOR, (el, value) => el.value = value, prefs.email);
-            await page.$eval(TEL_SELECTOR, (el, value) => el.value = value, prefs.tel);
-            await page.$eval(CITY_SELECTOR, (el, value) => el.value = value, prefs.city);
-            await page.$eval(ZIP_SELECTOR, (el, value) => el.value = value, prefs.zip);
-            await page.$eval(COUNTRY_SELECTOR, (el, value) => el.value = value, prefs.country);
-            await page.$eval(CARD_TYPE_SELECTOR, (el, value) => el.value = value, prefs.cardType);
-            await page.$eval(CARD_MONTH_SELECTOR, (el, value) => el.value = value, prefs.cardMonth);
-            await page.$eval(CARD_YEAR_SELECTOR, (el, value) => el.value = value, prefs.cardYear);
-            await page.$eval(CARD_VVAL_SELECTOR, (el, value) => el.value = value, prefs.cardVval);
+            //use evaluate instaed of eval$ because eval$ is not really working (i think because of the pseudo elements)
+            await page.evaluate((selector, value) => document.getElementById(selector).value = value, NAME_SELECTOR, prefs.name);
+            await page.evaluate((selector, value) => document.getElementById(selector).value = value, EMAIL_SELECTOR, prefs.email);
+            await page.evaluate((selector, value) => document.getElementById(selector).value = value, TEL_SELECTOR, prefs.tel);
+            await page.evaluate((selector, value) => document.getElementById(selector).value = value, CITY_SELECTOR, prefs.city);
+            await page.evaluate((selector, value) => document.getElementById(selector).value = value, ZIP_SELECTOR, prefs.zip);
+            await page.evaluate((selector, value) => document.getElementById(selector).value = value, COUNTRY_SELECTOR, prefs.country);
+            await page.evaluate((selector, value) => document.getElementById(selector).value = value, CARD_TYPE_SELECTOR, prefs.cardType);
+            await page.evaluate((selector, value) => document.getElementById(selector).value = value, CARD_MONTH_SELECTOR, prefs.cardMonth);
+            await page.evaluate((selector, value) => document.getElementById(selector).value = value, CARD_YEAR_SELECTOR, prefs.cardYear);
+            await page.evaluate((selector, value) => document.getElementById(selector).value = value, CARD_VVAL_SELECTOR, prefs.cardVval);
             await page.click(TERMS_SELECTOR);
-            await page.$eval(RECAPTCHA_RESPONSE_SELECTOR, (el, value) => el.value = value, captchaToken);
+            await page.evaluate((selector, value) => document.getElementById(selector).value = value, RECAPTCHA_RESPONSE_SELECTOR, captchaToken);
 
             if (!isTesting) {
-                await page.$eval(CARD_NUMBER_SELECTOR, (el, value) => el.value = value, prefs.cardNumber);
-                await page.$eval(ADRESS_SELECTOR, (el, value) => el.value = value, prefs.adress);
+                await page.evaluate((selector, value) => document.getElementById(selector).value = value, CARD_NUMBER_SELECTOR, prefs.cardNumber);
+                await page.evaluate((selector, value) => document.getElementById(selector).value = value, ADRESS_SELECTOR, prefs.adress);
             }
 
             await page.waitFor(3000);
